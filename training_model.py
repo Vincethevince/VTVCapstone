1fec50eb04f9c29708dfac2ce8ec1a2957d9c37f
vincethevince <vincent.blaser@gmail.com>
2024-05-22 15:37:11 +0200

    Adding Masking and earlystopping to training_model

--- training_model.py
+++ training_model.py
@@ -1,9 +1,10 @@
-from preprocessing import num_encoder_tokens, num_decoder_tokens, decoder_target_data, encoder_input_data, decoder_input_data, decoder_target_data, max_encoder_seq_length, max_decoder_seq_length
+from preprocessing import num_encoder_tokens, num_decoder_tokens, decoder_target_data, encoder_input_data, decoder_input_data, max_encoder_seq_length, max_decoder_seq_length
 
-from tensorflow import keras
 # Add Dense to the imported layers
-from keras.layers import Input, LSTM, Dense, Masking
-from keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Dense, Masking
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+
 import os
 # os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
@@ -17,15 +18,16 @@ epochs = 500
 
 # Encoder training setup
 encoder_inputs = Input(shape=(None, num_encoder_tokens))
-
+encoder_mask = Masking(mask_value=0)(encoder_inputs)
 encoder_lstm = LSTM(dimensionality, return_state=True)
-encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
+encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_mask)
 encoder_states = [state_hidden, state_cell]
 
 # Decoder training setup:
 decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_mask = Masking(mask_value=0)(decoder_inputs)
 decoder_lstm = LSTM(dimensionality, return_sequences=True, return_state=True)
-decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(decoder_mask, initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
@@ -35,8 +37,11 @@ training_model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 # Compile the model:
 training_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])# sample_weight_mode='temporal')
 
+# Early stopping callback
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
 # print("Training the model:\n")
 # Train the model:
-training_model.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size = batch_size, epochs = epochs, validation_split = 0.2)
+training_model.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size = batch_size, epochs = epochs, validation_split = 0.2, callbacks=[early_stopping])
 
 training_model.save('training_model.h5')
